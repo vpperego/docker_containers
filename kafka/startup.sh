397b1f72ab0ea6b2dbdee137abef5e366adadb15
root <root@DESKTOP-KE2JAOS.localdomain>
2020-12-05 14:48:00 -0300

    kafka working local

--- kafka/startup.sh
+++ kafka/startup.sh
@@ -2,11 +2,4 @@
 
 nohup $KAFKA/bin/zookeeper-server-start.sh $KAFKA/config/zookeeper.properties > zookeeper_output.log &
 sleep 5
-nohup $KAFKA/bin/kafka-server-start.sh $KAFKA/config/server.properties > kafka_output.log &
-sleep 5
-
-while :
-do
-	echo "Press [CTRL+C] to stop.."
-	sleep 10
-done
+$KAFKA/bin/kafka-server-start.sh $KAFKA/config/server.properties
